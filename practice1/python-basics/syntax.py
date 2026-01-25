d3e399c909418c3ef44501b8dfe0db2d175287a8
DinaErgali <dinaergali0211@gmail.com>
2026-01-25 17:51:32 +0500

    Add syntax.py

--- practice1/python-basics/syntax.py
+++ practice1/python-basics/syntax.py
@@ -1,4 +1,4 @@
-#!
+#1
 if 5 > 2:
  print("Five is greater than two!")  
 if 5 > 2:
@@ -15,5 +15,15 @@ print(y) #output string Hello World!
 #This is a comment.
 print("Hello, World!")
 
+#4 one statement
+print("Python is fun!") 
+
+#5 three statements
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+#6 one line
+print("Hello"); print("How are you?"); print("Bye bye!")
 
 
