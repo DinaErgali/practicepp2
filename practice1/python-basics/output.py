6844fa7d968955da68b1fd7d33d24bbbcaf35ca7
DinaErgali <dinaergali0211@gmail.com>
2026-01-25 18:07:22 +0500

    Add output.py

---
+++ practice1/python-basics/output.py
@@ -0,0 +1,19 @@
+# 1 - Use either double quotes or single quotes 
+print("This will work!")
+print('This will also work!')
+
+# 2 - Output without a new line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+
+# 3 - Output numbers
+print(3)
+print(358)
+print(50000)
+print(3 + 3)
+print(2 * 5)
+
+
+# 4 - Mix text and numbers 
+print("I am", 35, "years old.")
--- practice1/python-basics/syntax.py
+++ practice1/python-basics/syntax.py
@@ -1,29 +1,27 @@
-#1
+# 1 
 if 5 > 2:
- print("Five is greater than two!")  
-if 5 > 2:
-        print("Five is greater than two!") 
+    print("Five is greater than two!")
 
-#2
+if 5 > 2:
+        print("Five is greater than two!")
+
+# 2 
 x = 5
 y = "Hello, World!"
 
-print(x) #output integer 5
-print(y) #output string Hello World!
+print(x)  # output the integer 5
+print(y)  # output the string Hello, World!
 
-#3
-#This is a comment.
+# 3 - Comment example
 print("Hello, World!")
 
-#4 one statement
-print("Python is fun!") 
+# 4 - One statement
+print("Python is fun!")
 
-#5 three statements
+# 5 - Multiply statement
 print("Hello World!")
 print("Have a good day.")
 print("Learning Python is fun!")
 
-#6 one line
+# 6 - Multiple Statements in One Line (Not recommended, but syntax allows)
 print("Hello"); print("How are you?"); print("Bye bye!")
-
-
