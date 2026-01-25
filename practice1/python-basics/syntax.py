1517470c6bf8163bdc72b7a99f39ab841a6a2a1f
DinaErgali <dinaergali0211@gmail.com>
2026-01-25 17:26:02 +0500

    Add syntax.py

---
+++ practice1/python-basics/syntax.py
@@ -0,0 +1,19 @@
+#!
+if 5 > 2:
+ print("Five is greater than two!")  
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#2
+x = 5
+y = "Hello, World!"
+
+print(x) #output integer 5
+print(y) #output string Hello World!
+
+#3
+#This is a comment.
+print("Hello, World!")
+
+
+
