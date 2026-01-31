0eb9abad5dae63cf28cd3786083de2cb622868e6
DinaErgali <dinaergali0211@gmail.com>
2026-02-01 03:33:53 +0500

    Add for_loop.py

---
+++ practice2/loops/for_loop.py
@@ -0,0 +1,19 @@
+# Python For Loops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# Else in For Loop
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+# Nested Loops
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
